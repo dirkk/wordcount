a5b77534fc0ca2a1081a32e7d21dc9e5db6ce7c1
Dirk Ofori <dirk.ofori@forcam.com>
2023-04-12 13:05:44 +0200

    Initial commit

---
+++ src/main/java/org/example/WordCount.java
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Created by DOfori on 12.04.2023.
+//
+// Copyright (c) 2006 - 2023 FORCAM GmbH. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////
+
+package org.example;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCount {
+
+    public static class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+        @Override
+        public void map(Object key, Text value, Context context) {
+            // Text is the hadoop type to store textual information, e.g. a string
+            // IntWritable is the hadoop class to write/read integer values
+
+            // at the end you write the output using context.write(key, value);
+        }
+    }
+
+    public static class WourdCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) {
+            // at the end you write the output again using context.write(key, value);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setCombinerClass(WourdCountReducer.class);
+        job.setReducerClass(WourdCountReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
